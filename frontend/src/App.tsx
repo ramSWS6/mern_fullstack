ee9afb32de6ace734e71af5d1c7cb085c45bdabf
ramSWS6 <ramsankar@softworthsolutions.in>
2026-06-04 11:21:53 +0530

    Initial fullstack setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind Working
+      </h1>
+    </div>
+  )
+}
+
+export default App
