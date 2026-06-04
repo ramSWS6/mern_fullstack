0f54ea74924c02caac87a159e2f3506b3d5b007c
ramSWS6 <ramsankar@softworthsolutions.in>
2026-06-04 14:11:36 +0530

    Setup backend tooling

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-  # MERN Fullstack Starter
+MERN Fullstack
 
-## Frontend
+Frontend
+React
+Vite
+TypeScript
+Tailwind CSS
+shadcn/ui
 
-* React
-* Vite
-* TypeScript
-* Tailwind CSS
-* shadcn/ui
+Backend
+Node.js
+Express.js
+TypeScript
 
-## Backend
 
-* Node.js
-* Express.js
-* TypeScript
+Frontend Setup
+cd frontend
+npm install
+npm run dev
+
+Frontend runs on:
+
+http://localhost:5173
+Backend Setup
+cd backend
+npm install
+npm run dev
+
+Backend runs on:
+
+http://localhost:5000
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,22 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+
     languageOptions: {
       globals: globals.browser,
     },
+
+    ...js.configs.recommended,
+
+    plugins: {
+      react: pluginReact,
+      "react-hooks": reactHooks,
+    },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
   },
-])
+
+  ...tseslint.configs.recommended,
+];
